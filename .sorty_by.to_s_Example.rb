78504c4bde05a768ac5c0514bd0e8ab019a0ffbb
AndrePaliao <andre_paliao@hotmail.com>
2017-09-27 14:46:59 -0300

    Update and rename .sorty_by_Example.rb to .sorty_by.to_s_Example.rb

---
+++ .sorty_by.to_s_Example.rb
@@ -0,0 +1,13 @@
+puts "Enter with an phrase: "
+text = gets.chomp
+
+words = text.split
+
+frequencies = Hash.new(0)
+words.each { |word| frequencies[word]+=1}
+
+frequencies = frequencies.sort_by {|word, count| count}
+
+frequencies.reverse!
+
+frequencies.each { |word,count| puts word + " " + count.to_s}
